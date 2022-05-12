c5a61b63f21b307816cc81d1a0a75ce7496a7cfb
trieuvy793 <93860211+trieuvy793@users.noreply.github.com>
2022-05-12 21:03:07 +0700

    Add files via upload

---
+++ Bai099/Bai099.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include<cmath>
+#include<time.h>
+#include<iomanip>
+using namespace std;
+
+int ViTriLonNhat(int[], int);
+int TimChuSo(int[][500], int, int);
+void Nhap(int[][500], int&, int&);
+void Xuat(int[][500], int, int);
+
+int main() {
+	int arr[500][500];
+	int m, n;
+	cout << "Moi nhap so hang: ";
+	cin >> n;
+	cout << "Moi nhap so cot: ";
+	cin >> m;
+	Nhap(arr, n, m);
+	cout << "Ma tran sau khi nhap la: \n";
+	Xuat(arr, n, m);
+
+	cout << "Chu so xuat hien nhieu nhat trong mang la: " << TimChuSo(arr, n, m);
+
+	return 0;
+}
+
+int ViTriLonNhat(int a[], int n) {
+	int lc = 0;
+	for (int i = 0; i < n - 1; i++)
+		if (a[i] > a[lc])
+			lc = i;
+	return lc;
+}
+
+int TimChuSo(int a[][500], int n, int m) {
+	int dem[10] = { 0,0,0,0,0,0,0,0,0,0 };
+	for(int i=0; i<n; i++)
+		for (int j = 0; j < m; j++)
+		{
+			if (a[i][j] == 0)
+				dem[0]++;
+			int t = abs(a[i][j]);
+			while (t != 0)
+			{
+				int dv = t % 10;
+				dem[dv]++;
+				t /= 10;
+			}
+		}
+	return ViTriLonNhat(dem, 10);
+}
+
+void Nhap(int a[][500], int& n, int& m)
+{
+	srand(time(NULL));
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			a[i][j] = -100 + (rand() / (RAND_MAX / (100 - (-100))));
+}
+
+void Xuat(int a[][500], int n, int m)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cout << setw(8);
+			cout << setprecision(3);
+			cout << a[i][j];
+		}
+		cout << "\n";
+	}
+}
